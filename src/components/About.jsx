d554a18d9141f655a90fb4497e896ea45af9f79b
Clovis Carmezini <cloviscarmezini14@gmail.com>
2023-03-30 11:47:08 -0300

    feat: add section component to About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,6 +5,8 @@ import { styles } from '../styles'
 import { services } from '../constants'
 import { fadeIn, textVariant } from '../utils/motion'
 
+import { SectionWrapper } from '../hoc';
+
 const ServiceCard = ({ title, icon, index }) => {
   return (
     <Tilt
@@ -72,4 +74,4 @@ const About = () => {
   )
 }
 
-export default About
+export default SectionWrapper(About, "about")
