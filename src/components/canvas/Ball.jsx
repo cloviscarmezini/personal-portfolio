45238f07e1aeed9afcd08e15220daf2e79685436
Clovis Carmezini <cloviscarmezini14@gmail.com>
2023-03-30 17:21:36 -0300

    refact: change format of tech items

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -4,7 +4,6 @@ import { Canvas } from '@react-three/fiber';
 import {
   Decal,
   Float,
-  OrbitControls,
   Preload,
   useTexture
 } from '@react-three/drei';
@@ -20,20 +19,15 @@ const Ball = ({ imgUrl }) => {
       rotationIntensity={1}
       floatIntensity={2}
     >
-      <ambientLight intensity={0.25}/>
-      <directionalLight position={[ 0,0,0.05 ]}/>
+      <ambientLight intensity={0.5}/>
+      <directionalLight position={[ 0, -1, 0.15 ]}/>
       <mesh castShadow receiveShadow scale={2.75}>
-        <icosahedronGeometry args={ [ 1, 1 ]}/>
-        <meshStandardMaterial
-          color="#fff8eb"
-          polygonOffset
-          polygonOffsetFactor={-5}
-          flatShading
-        />
+        <sphereGeometry/>
+        <meshPhysicalMaterial metalness={1} roughness={0.3} color="#ffffff" emissive="#fff8eb" />
+
         <Decal
           position={[0, 0, 1]}
           rotation={[ 2 * Math.PI, 0, 6.25]}
-          flatShading
           map={decal}
         />
       </mesh>
@@ -44,15 +38,12 @@ const Ball = ({ imgUrl }) => {
 const BallCanvas = ({ icon }) => {
   return (
     <Canvas
-      frameLoop='demand'
+      frameloop='always'
       gl={{
         preserveDrawingBuffer: true
       }}
     >
       <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls
-          enableZoom={false}
-        />
         <Ball imgUrl={icon} />
       </Suspense>
 
