71abfe1598dc30f22f169ac932a0a5b6feea4cd3
Clovis Carmezini <cloviscarmezini14@gmail.com>
2023-11-11 18:15:44 -0300

    melhoria na sessão de portfolio

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -102,9 +102,17 @@ project
 const Works = () => {
   const [workType, setWorkType] = useState('personal');
 
+  function onChangeWorkType(worktype) {
+    setWorkType(worktype)
+    document
+    .getElementById('portfolio')
+    ?.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  }
+
   return (
     <>
       <motion.div
+        id="portfolio"
         variants={textVariant()}
       >
         <p className={styles.sectionSubText}>My work</p>
@@ -120,15 +128,15 @@ const Works = () => {
         </motion.p>
       </div>
 
-      <div className="mt-10">
-        <div className="flex justify-center">
+      <div className="mt-5 relative">
+        <div className="bg-primary flex justify-center sticky top-[75px] py-5 z-50">
           <div className="rounded-full hover:shadow-card bg-gradient-to-b from-blue-500 to-purple-500">
             <div className="bg-tertiary m-[1px] text-sm text-white leading-none rounded-full inline-flex">
-              <button onClick={() => setWorkType('personal')} className={`inline-flex items-center transition-colors duration-300 ease-in focus:outline-none hover:bg-blue-500 rounded-l-full px-4 py-2 ${workType === 'personal' ? 'bg-gradient-to-b from-blue-500 to-purple-500' : ''}`} id="grid">
+              <button onClick={() => onChangeWorkType('personal')} className={`inline-flex items-center transition-colors duration-300 ease-in focus:outline-none hover:bg-blue-500 rounded-l-full px-4 py-2 ${workType === 'personal' ? 'bg-gradient-to-b from-blue-500 to-purple-500' : ''}`} id="grid">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="fill-current w-4 h-4 mr-2"><rect x="3" y="3" width="7" height="7"></rect><rect x="14" y="3" width="7" height="7"></rect><rect x="14" y="14" width="7" height="7"></rect><rect x="3" y="14" width="7" height="7"></rect></svg>
                 <span>Individual work</span>
               </button>
-              <button onClick={() => setWorkType('collaborate')} className={`inline-flex items-center transition-colors duration-300 ease-in focus:outline-none hover:bg-blue-500 rounded-r-full px-4 py-2 ${workType === 'collaborate' ? 'bg-gradient-to-b from-blue-500 to-purple-500' : ''}`} id="list">
+              <button onClick={() => onChangeWorkType('collaborate')} className={`inline-flex items-center transition-colors duration-300 ease-in focus:outline-none hover:bg-blue-500 rounded-r-full px-4 py-2 ${workType === 'collaborate' ? 'bg-gradient-to-b from-blue-500 to-purple-500' : ''}`} id="list">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="fill-current w-4 h-4 mr-2"><line x1="8" y1="6" x2="21" y2="6"></line><line x1="8" y1="12" x2="21" y2="12"></line><line x1="8" y1="18" x2="21" y2="18"></line><line x1="3" y1="6" x2="3.01" y2="6"></line><line x1="3" y1="12" x2="3.01" y2="12"></line><line x1="3" y1="18" x2="3.01" y2="18"></line></svg>
                 <span>Collaborative work</span>
               </button>
